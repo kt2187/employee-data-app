06b992142d69b200f5515a061b4ae192b9903fc9
Katie Barriere <kbarriere@gmail.com>
2019-12-28 08:42:48 -0500

    Add employees

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/employeedata', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/mongo-exercises.employees', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.log('Could not connect to MongoDB...', err))
 
 const employeeDataSchema = new mongoose.Schema({
   employeeName: String,
-  employeeId: String,
+  department: String,
   dateOfBirth: String,
   startDate: String,
   currentSalary: String
 });
 
 const Employee = mongoose.model('Employee', employeeDataSchema);
+
+async function addEmployee() {
+  const employee = new Employee({
+    employeeName: 'Barriere, Katie',
+    department: '10006',
+    dateOfBirth: '1968-07-19',
+    startDate: '1995-11-2',
+    currentSalary: '56895'
+  });
+
+  const result = await employee.save();
+  console.log(result);
+}
+
+addEmployee();
